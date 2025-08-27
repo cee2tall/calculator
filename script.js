e0c67cbf85d58f21161a88292d820bbce11569ec
cee2tall <182031182+cee2tall@users.noreply.github.com>
2025-08-27 02:09:39 -0600

    Add functionality for calculating intermediate steps
    May contain errors and code could be cleaned up.

--- script.js
+++ script.js
@@ -12,29 +12,67 @@ function divide(a, b) {
 }
 
 function operate(left, right, operator) {
+  let leftNum = +left,
+    rightNum = +right;
   switch (operator) {
     case "+":
-      return add(left, right);
+      return add(leftNum, rightNum);
     case "-":
-      return subtract(left, right);
+      return subtract(leftNum, rightNum);
     case "*":
-      return multiply(left, right);
+      return multiply(leftNum, rightNum);
     case "/":
-      return divide(left, right);
+      return divide(leftNum, rightNum);
   }
 }
 
-// const numberButtons = document.querySelectorAll(".number");
-// numberButtons.array.forEach((element) => {});
-
+let left = 0,
+  right,
+  operator;
+let currStage = 1;
 const screen = document.querySelector("#screen");
 const smallButtons = document.querySelector("#small-buttons");
+const bigButtons = document.querySelector("#big-buttons");
+
 smallButtons.addEventListener("click", (event) => {
   let target = event.target;
   if (target.id === "small-buttons") {
     return;
+  } else if (target.id === "erase") {
+    if (screen.innerText.length > 0) {
+      screen.innerText = screen.innerText.slice(0, -1);
+    }
+    return;
+  } else if (target.classList.contains("operator")) {
+    if (operator !== undefined) {
+      screen.innerText = "" + operate(left, screen.innerText, operator);
+    }
+    left = screen.innerText;
+    operator = target.innerText;
+    currStage = 2;
+    console.log(`left: ${left} and operator ${operator}`);
+
+    return;
+  } else {
+    if (currStage === 2) {
+      screen.innerText = "";
+      currStage = 1;
+    }
+    screen.innerText += target.innerText;
   }
-  screen.innerText += target.innerText;
 });
 
-let left, right, operator;
+bigButtons.addEventListener("click", (event) => {
+  let target = event.target;
+  if (target.id === "clear") {
+    screen.innerText = "";
+    operator = undefined;
+    currStage = 1;
+  } else {
+    screen.innerText = "" + operate(left, screen.innerText, operator);
+  }
+  operator = undefined;
+});
+
+// "5 + 10 * 10 - 10"
+// "5 +(STAGE 2) 1(STAGE 3)0 * 10"
